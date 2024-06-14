c334e5b0568dffb806ca7bbd527bb987c83ab66a
Lucas Estrada <101399158+Kasssio@users.noreply.github.com>
2024-06-14 11:57:47 -0300

    Added __repr__

--- templates/campana_verde.py
+++ templates/campana_verde.py
@@ -2,8 +2,8 @@ from haversine import haversine, Unit
 class CampanaVerde:
     def __init__(self, direccion:str, barrio:str, comuna:int, materiales:set[str], latitud:float, longitud:float):
         '''
-        Inicializa un objeto de tipo CampanaVerde.
-        Requiere: nada.
+        Inicializa una campana verde con dirección direccion, barrio barrio, materiales materiales, 
+        latitud latitud y longitud longitud.
         '''
         self.direccion:str = direccion
         self.barrio:str = barrio
@@ -13,21 +13,15 @@ class CampanaVerde:
         self.longitud:float = longitud
 
     def distancia(self, lat2:float, lon2:float) -> ...:
-        '''
-        Requiere: nada.
-        Devuelve: la distancia entre la campana verde y la ubicación dada.
-        '''
+        ''' Devuelve la distancia entre la campana verde y la ubicación dada. '''
         punto1:tuple[float, float] = (self.latitud, self.longitud)
         punto2:tuple[float, float] = (lat2, lon2)
         return haversine(punto1, punto2, unit = Unit.METERS)
 
     def __repr__(self) -> str:
-        ''' completar docstring '''
-        #<AGUIRRE 1447@Papel/Cartón@CHACARITA>
-        return '<' + self.direccion + '@' + self.materiales.split(' ') + '@' + self.barrio + '>'
-        pass
+        ''' Devuelve una representación string de la campana verde, especificando dirección, materiales y barrio. '''
+        return '<' + self.direccion + '@' + '/'.join(self.materiales) + '@' + self.barrio + '>'
 
 camp:CampanaVerde = CampanaVerde('AGUIRRE 1447', 'CHACARITA', 15, {'Papel', 'Cartón'}, -58.4436445327415, -34.5893377789048)
 print(camp)
-#AGUIRRE 1447	CHACARITA	15	Papel / CartÃ³n	Los materiales deben estar limpios y secos
-#POINT (-58.4436445327415 -34.5893377789048)
+
