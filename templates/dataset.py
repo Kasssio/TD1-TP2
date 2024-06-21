c78d0b6114d2417d89ad242e5dfb0adb11e083c0
josedesgtt <josedesgtt@gmail.com>
2024-06-21 18:38:23 -0300

    "Josefina > Lucas"

--- templates/dataset.py
+++ templates/dataset.py
@@ -26,6 +26,7 @@ class DataSetCampanasVerdes:
         Requiere: nada.
         Devuelve: la cantidad de campanas del dataset.
         '''
+        # Simplemente debo sacar la longitud de self.campanas, que contiene todas las campanas del dataset
         return len(self.campanas)
 
     def barrios(self) -> set[str]:
@@ -36,17 +37,17 @@ class DataSetCampanasVerdes:
         barrios:set[str] = set() # Inicializo el conjunto
         for campana in self.campanas: # Por cada campana
             barrios.add(campana.barrio) # Añado su barrio al conjunto (no se repiten gracias a las propiedades de los conjuntos)
-        return barrios
+        return barrios # Devuelvo el conjunto de barrios
 
     def campanas_del_barrio(self,barrio:str) -> list[CampanaVerde]:
         ''' 
         Requiere: nada.
         Devuelve: una lista con las campanas verdes que hay en un barrio.
         '''
-        vr:list[CampanaVerde] = []
-        for campana in self.campanas:
-            if campana.barrio == barrio:
-                vr.append(campana)
+        vr:list[CampanaVerde] = [] # Acá se guardarán las campanas
+        for campana in self.campanas: # Por cada campana del dataset
+            if campana.barrio == barrio: # Si la campana pertenece a ese barrio...
+                vr.append(campana) # ...la añadimos a la lista!
         return vr
 
     def cantidad_por_barrio(self,material) -> dict[str,int]:
@@ -54,27 +55,27 @@ class DataSetCampanasVerdes:
         Requiere: nada.
         Devuelve: un diccionario con la cantidad de campanas verdes en un barrio en las que se puede depositar material.
         '''
-        vr:dict[str,int] = {}
-        barrios = self.barrios() 
-        for barrio in barrios:
-            vr[barrio] = 0
-            campanas = self.campanas_del_barrio(barrio)
-            for i in campanas: # O(N)
-                if material in i.materiales:
-                    vr[barrio] += 1
-        return vr
+        vr:dict[str,int] = {} # Iniciamos un diccionario donde a cada barrio le asociamos la cantidad de campanas en las cuales podemos poner el material especificado
+        barrios = self.barrios() # Guardamos los barrios existentes en el dataset
+        for barrio in barrios: # Por cada barrio
+            vr[barrio] = 0 # Creamos su clave en el diccionario
+            campanas = self.campanas_del_barrio(barrio) # Extraemos todas las campanas presentes en ese barrio
+            for campana in campanas: # Por cada una de esas campanas
+                if material in campana.materiales: #Si en esa campana se puede tirar el material especificado
+                    vr[barrio] += 1 # Agregamos una campana al contador
+        return vr # Devolvemos el diccionario
     
     def ordenar_lista_de_tres(self,campana:CampanaVerde,lista:list[CampanaVerde],lat:float,lon:float):
         '''
         Requiere: len(lista) = 3.
         Devuelve: una lista de 3 campanas ordenadas en base a su cercanía a una coordenada.
         '''
-        if (lista[0].distancia(lat,lon) > campana.distancia(lat,lon)):
-            lista[0], lista[1], lista[2] = campana, lista[0], lista[1]
-        elif (lista[1].distancia(lat,lon) > campana.distancia(lat,lon)):
-            lista[1], lista[2] = campana, lista[1]
-        elif (lista[2].distancia(lat,lon) > campana.distancia(lat,lon)):
-            lista[2] = campana
+        if (lista[0].distancia(lat,lon) > campana.distancia(lat,lon)): # Si la campana actual tiene una distancia menor a la primera campana de la lista
+            lista[0], lista[1], lista[2] = campana, lista[0], lista[1] # La pone como primer elemento y mueve las demás campanas una posición hacia la derecha
+        elif (lista[1].distancia(lat,lon) > campana.distancia(lat,lon)): # Si la campana actual tiene una distancia mayor a la primera pero menor a la segunda
+            lista[1], lista[2] = campana, lista[1] # La pone como segundo elemento y mueve la siguiente campana una posición hacia la derecha
+        elif (lista[2].distancia(lat,lon) > campana.distancia(lat,lon)): # Si la campana actual tiene una distancia que es menor sólamente a la de la última
+            lista[2] = campana # Reemplaza la última posición por la campana actual
 
     def tres_campanas_cercanas(self,lat:float,lon:float) -> tuple[CampanaVerde,CampanaVerde,CampanaVerde]:
         '''
@@ -92,9 +93,9 @@ class DataSetCampanasVerdes:
             camps_cerca[0], camps_cerca[2] = camps_cerca[2], camps_cerca[0]
         if (camps_cerca[1].distancia(lat,lon) > camps_cerca[2].distancia(lat,lon)):
             camps_cerca[1], camps_cerca[2] = camps_cerca[2], camps_cerca[1]
-        for campana in self.campanas: # Voy agregando a la lista campanas más cercanas
-            self.ordenar_lista_de_tres(campana, camps_cerca, lat, lon)
-        return camps_cerca
+        for campana in self.campanas: # Por cada campana en el dataset
+            self.ordenar_lista_de_tres(campana, camps_cerca, lat, lon) # Llamo a la función que definí antes para ordenarla dentro de la lista
+        return camps_cerca # Devuelve las campanas cercanas
     
     def exportar_por_materiales(self, archivo_csv:str, materiales:set):
         '''  genera un nuevo archivo con
@@ -102,11 +103,11 @@ class DataSetCampanasVerdes:
         depositar todos los materiales del conjunto materiales, conjunto indicado como input del
         método. El archivo generado contiene únicamente las columnas DIRECCION y BARRIO .
         '''
-        f = open(archivo_csv, 'w', encoding='utf-8')
-        f.write("DIRECCION;BARRIO\n")
-        for campana in self.campanas:
-            if materiales & campana.materiales == materiales:
-                f.write(campana.direccion + ";" + campana.barrio + "\n")
+        f = open(archivo_csv, 'w', encoding='utf-8') # Abro un archivo nuevo
+        f.write("DIRECCION;BARRIO\n") # Le agrego el encabezado para identificar cada columna
+        for campana in self.campanas: # Por cada campana del dataset
+            if materiales & campana.materiales == materiales: # Si en esa campana se pueden tirar todos los materiales especificados
+                f.write(campana.direccion + ";" + campana.barrio + "\n") # La añadimos al archivo
 
 dataset = DataSetCampanasVerdes('templates/campanas-verdes.csv')
 dataset.tres_campanas_cercanas(-58.388208228334115, -34.610217535930296)
